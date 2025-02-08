8921c59eeaaab01b7d39c65494fd36f2f017acc4
DanielHerrera405 <daniel.herrera1@catolica.edu.sv>
2025-02-08 12:00:30 -0600

    fixed #3 Daniel Herrera: Estructura html noticia 2

---
+++ portalNoticiasGDJB/Pages/Noticia 2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace portalNoticiasGDJB.Pages
+{
+    public class Noticia_2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
