9a83602cb262179c6ded5c4ea644fb78f1bdba34
gabrielaflores4 <gabriela.flores9@catolica.edu.sv>
2025-02-19 17:17:48 -0600

    fixed #12 Gabriela Flores: Perfil de Usuario

---
+++ portalNoticiasGDJB/Pages/perfil.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace portalNoticiasGDJB.Pages
+{
+    public class perfilModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
