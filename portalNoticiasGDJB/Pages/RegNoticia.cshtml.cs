e2f9e94910f1aa19ea33cb0fb4a053e02ce62bb6
Brandon200310 <brandon.molina@catolica.edu.sv>
2025-02-19 22:54:02 -0600

    fixed #14 Brandon Molina: Registro de Noticia

---
+++ portalNoticiasGDJB/Pages/RegNoticia.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace portalNoticiasGDJB.Pages
+{
+    public class RegNoticiaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
